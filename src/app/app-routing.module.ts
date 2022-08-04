44d7fd9d6484272508856196a0ec5223193b9859
Nicolas Cettra <47118613+ncettra@users.noreply.github.com>
2022-08-04 12:37:43 -0300

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/body/home/home.component';
+import { OtroComponent } from './components/body/otro/otro.component';
+import { IndexProductsComponent } from './components/body/products/index-products/index-products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'otro', component: OtroComponent },
+  { path: 'productos', component: IndexProductsComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexProductsComponent } from './components/body/products/index-products/index-products.component';
+
+import { HomeComponent } from './components/body/home/home.component';
+import { BodyComponent } from './components/body/body.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/header/menu/menu.component';
+import { FormsModule } from '@angular/forms'; //ESTO ES CLAVE PARA QUE FUNCIONEN LOS FORMULARIOS
+import { HttpClientModule } from '@angular/common/http';
+import { OtroComponent } from './components/body/otro/otro.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexProductsComponent,
+    HomeComponent,
+    BodyComponent,
+    HeaderComponent,
+    MenuComponent,
+    OtroComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule, //<--
+
   ],
   providers: [],
   bootstrap: [AppComponent]
